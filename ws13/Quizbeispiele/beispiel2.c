b1c7b94ef665b366cdae668feb5cc54a2c426714
thomasneff <thomasneff93@gmail.com>
2013-11-10 15:49:12 +0100

    Added quiz file for switch/case

---
+++ ws13/Quizbeispiele/beispiel2.c
@@ -0,0 +1,16 @@
+
+int main()
+{
+  int a = 3;
+  
+  switch(a)
+  {
+    case 0: printf("Null\n");
+    case 1: printf("Eins\n");
+    case 2: printf("Zwei\n");
+    case 3: printf("Drei\n");
+    default: printf("Error");
+  }
+
+  return 0;
+}
